c23ae6fa108158fe11f587e794a1a1977572c495
PHANAGUS <phanarin03072002@gmail.com>
2021-12-28 16:49:06 +0700

    lab14_1.cpp completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,45 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[], int n, double b[])
+{
+    double sum = 0, sum2 = 0, sum3 = 0, sumlog = 0;
+    double max = a[0], min = a[0], mean, sd, gm, hm;
+
+    for(int i=0 ; i<n ; i++)
+    {
+        sum += a[i];
+    }
+    mean = sum/n;
+    b[0] = mean;
+
+    for(int i=0 ; i<n ;i++)
+    {
+        sum2 += pow((a[i]-mean),2);
+    }
+    sd = sqrt(sum2/n);
+    b[1] = sd;
+
+    for(int i=0 ; i<n ; i++)
+    {
+        sumlog += log10(a[i]);
+    }
+    gm = pow(10,sumlog/n);
+    b[2] = gm;
+
+    for(int i=0 ; i<n ; i++)
+    {
+        sum3 += (1/a[i]);
+    }
+    hm = n/sum3;
+    b[3] = hm;
+
+    for(int i=1 ; i<n ; i++)
+    {
+        if(a[i] > max) max = a[i];
+        if(a[i] < min) min = a[i];
+    }
+    b[4] = max;
+    b[5] = min;
+}
